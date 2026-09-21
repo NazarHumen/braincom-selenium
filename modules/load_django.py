a4fc7b7be4d2404786fbf476decc90e3f72d6bce
NazarHumen <nazargumen172@gmail.com>
2026-09-21 13:59:12 +0300

    feat: add standalone Django loader script for parser modules

---
+++ modules/load_django.py
@@ -0,0 +1,7 @@
+import os
+import sys
+import django
+
+sys.path.append("D:/company-sipius/braincom_selenium_project")
+os.environ["DJANGO_SETTINGS_MODULE"] = "braincom_project.settings"
+django.setup()
