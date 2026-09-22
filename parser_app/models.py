b15872961e45c23a04b3312d29b7dfd330dcc7f9
NazarHumen <nazargumen172@gmail.com>
2026-09-22 13:26:41 +0300

    feat: add Product model for parsed product data

--- parser_app/models.py
+++ parser_app/models.py
@@ -1,3 +1,21 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=500, null=True)
+    color = models.CharField(max_length=255, null=True)
+    memory = models.CharField(max_length=255, null=True)
+    manufacturer = models.CharField(max_length=255, null=True)
+    price = models.CharField(max_length=255, null=True)
+    sale_price = models.CharField(max_length=255, null=True)
+    product_code = models.CharField(max_length=255, null=True)
+    reviews_count = models.IntegerField(null=True)
+    screen_diagonal = models.CharField(max_length=255, null=True)
+    screen_resolution = models.CharField(max_length=255, null=True)
+
+    images = ArrayField(models.URLField(max_length=500, null=True), null=True)
+    characteristics = models.JSONField(null=True, blank=True)
+
+    search_query = models.CharField(max_length=255, null=True)
+    link = models.URLField(max_length=500, null=True)
